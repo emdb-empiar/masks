67162cbe5085d6c75ebe4d4b09dcf465d265d34d
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2020-11-30 17:03:31 +0000

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='masks',
     version='0.1.0',
-    packages=[''],
+    packages=find_packages(),
     url='',
     license='Apache Public License',
     author='Paul K. Korir, PhD',
